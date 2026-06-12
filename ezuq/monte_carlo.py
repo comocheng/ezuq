7ac788f9e955dd0293980cc12b0d9abed4eadfcc
Sevy Harris <sevy.harris@gmail.com>
2026-06-12 15:05:37 -0400

    add uncorrelated monte carlo

--- ezuq/monte_carlo.py
+++ ezuq/monte_carlo.py
@@ -2,6 +2,7 @@
 
 
 import os
+
 import sys
 import pickle
 import shutil
@@ -10,17 +11,21 @@ import yaml
 import cantera as ct
 import rmgpy.chemkin
 
+import SALib.analyze.morris
+
 import ezuq.util
 from ezuq.simulation.jsr import run_simulation
 
 CHUNK_SIZE = 1000
 
-def setup_runfiles(working_dir, conditions, morris_dir=None):
+def setup_runfiles(working_dir, conditions, morris_dir=None, i_sens=None):
     """Set up the runfiles for Monte Carlo Sampling
     working_dir should be the directory where the RMG and Cantera mechanisms are saved.
 
     optional morris_dir to use the results of screening
     if no morris_dir is provided, this will set up the runfiles for a full Monte Carlo sampling, which is probably too expensive to run to convergence
+
+    i_sens is the index of the output variable to use for screening if using morris_dir
     """
 
     monte_carlo_dir = os.path.join(working_dir, 'monte_carlo')
@@ -47,22 +52,72 @@ def setup_runfiles(working_dir, conditions, morris_dir=None):
     assert gas.n_reactions == len(ct2rmg_rxn), "Kinetic covariance matrix size does not match number of reactions in Cantera mechanism"
     assert len(set(ct2rmg_rxn.values())) == len(reaction_list), "Reactions in Cantera mechanism do not match reactions in RMG mechanism"
 
-    if morris_dir is None:
-        # Define the problem using SALib format
-        problem = {
-            'num_vars': len(species_list) + len(reaction_list),
-            'names': [sp.to_chemkin() for sp in species_list] + 
-                    [rxn.to_chemkin(species_list, kinetics=False) for rxn in reaction_list],
-        }
-        with open(os.path.join(monte_carlo_dir, 'problem_desc.yaml'), 'w') as f:
-            yaml.dump(problem, f)
+    # make the condition dirs, use the input conditions to name things
+    ezuq.util.setup_condition_dirs(monte_carlo_dir, conditions)
+    if morris_dir and i_sens is not None:
+        morris_samples = np.load(os.path.join(morris_dir, 'morris_samples.npy'))
+        with open(os.path.join(morris_dir, 'problem_desc.yaml'), 'rb') as f:
+            problem = pickle.load(f)
 
-        ezuq.util.setup_condition_dirs(monte_carlo_dir, conditions)
 
-        # copy the slurm script into the Monte Carlo dir
-        shutil.copyfile(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'scripts', 'SLURM', 'run_monte_carlo.sh'), os.path.join(monte_carlo_dir, 'run_monte_carlo.sh'))
-    else:
-        raise NotImplementedError("Setting up runfiles from a previous Morris screening is not implemented yet")
+        # match the condition dir to the morris condition dir through the name.
+        for condition in conditions:
+            morris_settings_yaml = os.path.join(morris_dir, condition["name"], 'settings.yaml')
+            if not os.path.exists(morris_settings_yaml):
+                print(f'Skipping condition {condition["name"]} since no matching Morris settings file found')
+                continue
+            morris_y_file = os.path.join(morris_dir, condition["name"], 'morris_sim_results.npy')
+            if not os.path.exists(morris_y_file):
+                print(f'Skipping condition {condition["name"]} since no matching Morris results file found')
+                continue
+
+            morris_y = np.load(morris_y_file)
+            Si = SALib.analyze.morris.analyze(problem, morris_samples, morris_y[:, i_sens])
+
+            # Rank parameter names by mean effect
+            contributions = [(x, y) for y, x in sorted(zip(Si['mu_star'].data, problem['names']))][::-1]
+
+            # define the tolerance for considering a parameter to be irrelevant
+            threshold = 0.05 * contributions[0][1]  # use 0.01 for tighter tolerance
+            k_params = []
+            g_params = []
+            
+            species_names = [x.to_chemkin() for x in species_list]
+            reaction_names = [x.to_chemkin(species_list, kinetics=False) for x in reaction_list]
+            for i in range(len(contributions)):
+                name = contributions[i][0]
+                if name in species_names:
+                    g_params.append(species_names.index(name))
+                elif name in reaction_names:
+                    k_params.append(reaction_names.index(name))
+                else:
+                    raise ValueError(f'could not identify parameter with name {name}')
+                
+                if contributions[i][1] < threshold:
+                    print(f'Reduced to {i} params')
+                    break
+
+            # save the problem description for this condition. Different conditions will have difference reduced parameter sets
+            monte_carlo_condition_dir = os.path.join(monte_carlo_dir, os.path.basename(condition["name"]))
+            problem = {
+                'g_params': g_params,
+                'k_params': k_params,
+                'num_vars': len(g_params) + len(k_params),
+                'g_param_names': [species_list[i].to_chemkin() for i in g_params],
+                'k_param_names': [reaction_list[i].to_chemkin(species_list, kinetics=False) for i in k_params],
+            }
+
+            with open(os.path.join(monte_carlo_condition_dir, 'problem_desc.yaml'), 'w') as f:
+                yaml.dump(problem, f, default_flow_style=False)
+            
+            with open(os.path.join(monte_carlo_condition_dir, 'settings.yaml'), 'w') as f:
+                yaml.dump(condition, f, default_flow_style=False)
+
+    # if no Morris dir is provided, we'll run a full Monte Carlo sampling, which requires no problem description since all parameters are varied
+
+    # copy the slurm script into the Monte Carlo dir
+    shutil.copyfile(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'scripts', 'SLURM', 'run_monte_carlo.sh'), os.path.join(monte_carlo_dir, 'run_monte_carlo.sh'))
+
 
 def run_chunk(settings_yaml, chunk_index):
     """Run a chunk of the morris simulations
@@ -117,53 +172,82 @@ def run_chunk(settings_yaml, chunk_index):
     assert gas.n_reactions == len(ct2rmg_rxn), "Kinetic covariance matrix size does not match number of reactions in Cantera mechanism"
     assert len(set(ct2rmg_rxn.values())) == len(reaction_list), "Reactions in Cantera mechanism do not match reactions in RMG mechanism"
 
-    with open(os.path.join(monte_carlo_dir, 'problem_desc.yaml'), 'r') as f:
-        problem = yaml.load(f, Loader=yaml.FullLoader)
-
-
     y = np.zeros((CHUNK_SIZE, gas.n_species))
-    if problem['num_vars'] == len(species_list) + len(reaction_list):
-        # no reduction was done, so this will be a full Monte Carlo sampling
+
+    # save copies of all thermo for faster perturbation
+    thermo_copies = []
+    for sp_index in range(gas.n_species):
+        thermo_copies.append(ct.Species().from_dict(gas.species()[sp_index].input_data.copy()))
+
+    problem_desc_file = os.path.join(condition_dir, 'problem_desc.yaml')
+    if not os.path.exists(problem_desc_file):
+        # no problem dscription with parameter reduction was done, so this will be a full Monte Carlo sampling
+        g_params = list(range(gas.n_species))
+        k_params = list(range(len(reaction_list)))
 
         thermo_perturbations = np.random.multivariate_normal(mean=np.zeros(thermo_covariance_matrix.shape[0]), cov=thermo_covariance_matrix, size=CHUNK_SIZE) * 4184  # convert to J/mol
         kinetic_perturbations = np.random.multivariate_normal(mean=np.zeros(kinetic_covariance_matrix.shape[0]), cov=kinetic_covariance_matrix, size=CHUNK_SIZE)
-        kinetic_multipliers = np.exp(kinetic_perturbations)
-        kinetic_multipliers_ct = kinetic_multipliers.dot(ct2rmg_matrix.T)
-
-        # save copies of all thermo for faster perturbation
-        thermo_copies = []
-        for sp_index in range(gas.n_species):
-            thermo_copies.append(ct.Species().from_dict(gas.species()[sp_index].input_data.copy()))
-
-        # Cantera does well if you give it lots of CPUs for a single simulation
-        # but slows down if you try to parallelize different simulations across multiple processes
-        # so we run the simulations in serial here do the parallelize across SLURM array jobs.
-        for i in range(CHUNK_SIZE):
-
-            # perturb all the species
-            for sp_index in range(gas.n_species):
-                # random perturbation
-                perturbed_sp = ezuq.util.perturb_species_ct(gas.species()[sp_index], thermo_perturbations[i, sp_index])
-                gas.modify_species(sp_index, perturbed_sp)
-
-            # set multipliers
-            for rxn_index_ct in range(gas.n_reactions):
-                gas.set_multiplier(kinetic_multipliers_ct[i, rxn_index_ct], rxn_index_ct)
-            try:
-                # TODO add timeout here so that if a simulation is taking too long we can skip it and move on 
-                y[i, :] = run_simulation(gas, settings)
-            except ct.CanteraError:
-                y[i, :] = np.nan  # if the simulation fails, we can fill in NaNs and move on. The Morris analysis can handle some failed simulations as long as most of them work.
-
-            # Reset things
-            for sp_index in range(gas.n_species):
-                gas.modify_species(sp_index, thermo_copies[sp_index])
-            gas.set_multiplier(1.0)
-
-        np.save(output_filename, y)
-
+    
     else:
-        raise NotImplementedError("Loading a problem description from a previous Morris screening is not implemented yet")
+        with open(os.path.join(monte_carlo_dir, 'problem_desc.yaml'), 'r') as f:
+            problem = yaml.load(f, Loader=yaml.FullLoader)
+
+        g_params = problem['g_params']
+        k_params = problem['k_params']
+
+        # This is only okay if there are no off-diagonals
+        # count the off_diagonals
+        off_diagonals = np.sum(thermo_covariance_matrix != np.diag(np.diagonal(thermo_covariance_matrix))) + np.sum(kinetic_covariance_matrix != np.diag(np.diagonal(kinetic_covariance_matrix)))
+        if off_diagonals > 0:
+            raise NotImplementedError("Parameter reduction with non-diagonal covariance matrices is not implemented yet.")
+
+        # reduce the thermo covariance matrix to the species in g_params
+        thermo_covariance_matrix_subset = thermo_covariance_matrix[np.ix_(g_params, g_params)]
+        kinetic_covariance_matrix_subset = kinetic_covariance_matrix[np.ix_(k_params, k_params)]
+
+        thermo_perturbations_subset = np.random.multivariate_normal(mean=np.zeros(thermo_covariance_matrix_subset.shape[0]), cov=thermo_covariance_matrix_subset, size=CHUNK_SIZE) * 4184  # convert to J/mol
+        kinetic_perturbations_subset = np.random.multivariate_normal(mean=np.zeros(kinetic_covariance_matrix_subset.shape[0]), cov=kinetic_covariance_matrix_subset, size=CHUNK_SIZE)
+
+        thermo_perturbations = np.zeros((CHUNK_SIZE, gas.n_species))
+        kinetic_perturbations = np.zeros((CHUNK_SIZE, len(reaction_list)))
+
+        for i, g_param in enumerate(g_params):
+            thermo_perturbations[:, g_param] = thermo_perturbations_subset[:, i]
+        for j, k_param in enumerate(k_params):
+            kinetic_perturbations[:, k_param] = kinetic_perturbations_subset[:, j]
+
+    kinetic_multipliers = np.exp(kinetic_perturbations)
+    kinetic_multipliers_ct = kinetic_multipliers.dot(ct2rmg_matrix.T)
+
+
+    # Cantera does well if you give it lots of CPUs for a single simulation
+    # but slows down if you try to parallelize different simulations across multiple processes
+    # so we run the simulations in serial here do the parallelize across SLURM array jobs.
+    for i in range(CHUNK_SIZE):
+
+        # perturb all the species
+        for sp_index in g_params:
+            # random perturbation
+            perturbed_sp = ezuq.util.perturb_species_ct(gas.species()[sp_index], thermo_perturbations[i, sp_index])
+            gas.modify_species(sp_index, perturbed_sp)
+
+        # set multipliers
+        for j, rxn_index_rmg in enumerate(k_params):
+            ct_indices = np.where(ct2rmg_matrix[:, rxn_index_rmg] == 1)[0]
+            for ct_index in ct_indices:
+                gas.set_multiplier(kinetic_multipliers_ct[i, ct_index], ct_index)
+        try:
+            # TODO add timeout here so that if a simulation is taking too long we can skip it and move on 
+            y[i, :] = run_simulation(gas, settings)
+        except ct.CanteraError:
+            y[i, :] = np.nan  # if the simulation fails, we can fill in NaNs and move on. The Morris analysis can handle some failed simulations as long as most of them work.
+
+        # Reset things
+        for sp_index in g_params:
+            gas.modify_species(sp_index, thermo_copies[sp_index])
+        gas.set_multiplier(1.0)
+
+    np.save(output_filename, y)
 
 
 if __name__ == "__main__":
