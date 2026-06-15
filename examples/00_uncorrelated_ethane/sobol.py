89835b111f8be10079990ffd1a6e3eaae99c893c
Sevy Harris <sevy.harris@gmail.com>
2026-06-15 17:42:11 -0400

    save sobol indepedent reduction demo

---
+++ examples/00_uncorrelated_ethane/sobol.py
@@ -0,0 +1,218 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.17.2
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# # Demo Notebook for Sobol Sampling with Model Reduction
+
+# +
+import os
+import pickle
+import yaml
+import subprocess
+import cantera as ct
+
+import rmgpy.chemkin
+import scipy.stats
+import numpy as np
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+import ezuq.sobol
+import ezuq.morris_screen
+import ezuq.util
+import ezuq.simulation
+
+import SALib.analyze.sobol
+# -
+
+working_dir = os.path.abspath('.')
+gas = ct.Solution(os.path.join(working_dir, 'chem_annotated.yaml'))
+
+# +
+# Specify conditions for the simulation
+temperatures = [800, 900, 1000]
+
+i_C2H6 = ezuq.util.get_i_thing_ct({'C': 2, 'H': 6}, gas)
+i_O2 = ezuq.util.get_i_thing_ct({'O': 2}, gas)
+i_Ar = ezuq.util.get_i_thing_ct({'Ar': 1}, gas)  # He for Nancy data
+x_C2H6 = 0.18
+x_O2 = 0.64
+x_Ar = 1.0 - x_C2H6 - x_O2
+X = f'{gas.species_names[i_C2H6]}: {x_C2H6}, {gas.species_names[i_O2]}: {x_O2}, {gas.species_names[i_Ar]}: {x_Ar}'
+
+conditions = []
+for T in temperatures:
+    conditions.append({
+        'name': f'{T}K',        # name for the directory where analysis will be run
+        'temperature': T,       # K
+        'pressure': ct.one_atm, # Pa
+        'composition': X,
+        'residence_time': 6.0,  # s
+        'volume': 9.5e-5,       # m^3
+    })
+
+# Here we reduce the number of samples for much faster runtime, but you'll probably want to do ~1024
+ezuq.sobol.setup_runfiles(working_dir, conditions, i_sens=14, N=256)
+# -
+
+# # RUN THE SIMS
+
+for condition in conditions:
+    condition_dir = os.path.join(working_dir, 'sobol', condition['name'])
+
+    my_settings_file = os.path.join(condition_dir, 'settings.yaml')
+    print(f'Running sims for {condition["name"]}')
+    for i in range(6):
+        
+        subprocess.check_call(['python', '-m', 'ezuq.sobol', my_settings_file, str(i)])
+
+for condition in conditions:
+    condition_dir = os.path.join(working_dir, 'sobol', condition['name'])
+    ezuq.sobol.reassemble_chunks(condition_dir)
+
+# # Plot the distribution
+
+# Get nominal results for plotting
+i_sp = 14
+cantera_file = os.path.join(working_dir, 'chem_annotated.yaml')
+gas = ct.Solution(cantera_file)
+nominal_results = np.zeros((len(conditions), gas.n_species))
+for z, condition in enumerate(conditions):
+    nominal_results[z, :] = ezuq.simulation.jsr.run_simulation(gas, condition)
+
+# Get upper/lower 95% confidence intervals for plotting
+upper95 = np.zeros((len(conditions), gas.n_species))
+lower95 = np.zeros((len(conditions), gas.n_species))
+for z, condition in enumerate(conditions):
+    name = condition['name']
+    condition_dir = os.path.join(working_dir, 'sobol', name)
+    sobol_samples = np.load(os.path.join(condition_dir, 'sobol_results.npy'))
+    lower95[z, :] = np.percentile(sobol_samples, 2.5, axis=0)
+    upper95[z, :] = np.percentile(sobol_samples, 97.5, axis=0)
+
+# +
+colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+for z, condition in enumerate(conditions):
+    name = condition['name']
+    temperature = condition['temperature']
+    condition_dir = os.path.join(working_dir, 'sobol', name)
+    sobol_samples = np.load(os.path.join(condition_dir, 'sobol_results.npy'))
+
+    # remove all zeros
+    nonzero_data = sobol_samples[~np.all(sobol_samples == 0, axis=1)]
+
+    result = plt.hist(nonzero_data[:, i_sp], 48, density=True, alpha=0.6)
+    plt.axvline(x=np.mean(nonzero_data[:, i_sp]), color=colors[0], label='Mean')
+    plt.axvline(x=np.median(nonzero_data[:, i_sp]), color='black', label='Median')
+    plt.axvline(x=nominal_results[z, i_sp], color='red', linestyle='dashed', label='Nominal Value')
+
+
+    plt.title(f'{gas.species_names[i_sp]} PDF - {name}')
+    plt.xlabel('Mole Fraction')
+    plt.ylabel('Density')
+    plt.legend()
+    plt.show()
+# -
+
+# # Plot the final model with errorbars
+
+# +
+
+temperatures = [c['temperature'] for c in conditions]
+plt.plot(temperatures, nominal_results[:, i_sp], label='Nominal Value')
+for z, condition in enumerate(conditions):
+    name = condition['name']
+    temperature = condition['temperature']
+    condition_dir = os.path.join(working_dir, 'sobol', name)
+    sobol_samples = np.load(os.path.join(condition_dir, 'sobol_results.npy'))
+
+    # remove all zeros
+    nonzero_data = sobol_samples[~np.all(sobol_samples == 0, axis=1)]
+    
+    # show 95% confidence interval
+    label = '_no_label'
+    if z == 0:
+        label = '95% Confidence Interval'
+    plt.fill_between(temperatures, lower95[:, i_sp], nominal_results[:, i_sp], alpha=0.1, color=colors[0], label=label)
+    plt.fill_between(temperatures, nominal_results[:, i_sp], upper95[:, i_sp], alpha=0.1, color=colors[0])
+    
+
+    plt.boxplot(
+        [nonzero_data[:, i_sp]],
+        positions=[temperature],
+        widths=5,
+        showfliers=False,
+        # patch_artist=True,
+        medianprops=dict(color='black')
+    )
+plt.title(f'{gas.species_names[i_sp]} Concentration')
+plt.xlabel('Temperature (K)')
+plt.ylabel('Mole Fraction')
+plt.legend()
+# -
+
+print(lower95)
+
+# # Look at some rankings
+
+# +
+# Load the Sobol X
+sobol_X = np.load(os.path.join(working_dir, 'sobol', conditions[2]['name'], 'sobol_samples.npy'))
+with open(os.path.join(working_dir, 'sobol', conditions[2]['name'], 'problem_desc.yaml'), 'rb') as f:
+    sobol_problem = yaml.load(f, Loader=yaml.FullLoader)
+
+sobol_y = np.load(os.path.join(working_dir, 'sobol', conditions[2]['name'], 'sobol_results.npy'))
+
+problem_desc_file = os.path.join(working_dir, 'sobol', conditions[2]['name'], 'problem_desc.yaml')
+with open(problem_desc_file, 'r') as f:
+    problem = yaml.load(f, Loader=yaml.FullLoader)
+
+# -
+
+
+
+sobol_problem = {
+    'num_vars': problem['num_vars'],
+    'bounds': problem['bounds'],
+    'names': problem['names'],
+}
+
+Si = SALib.analyze.sobol.analyze(sobol_problem, sobol_y[:, i_sp], calc_second_order=False, seed=400)
+
+# +
+# Show S1 rankings
+
+S1_results = [(name, x) for x, name in sorted(zip(Si['S1'], sobol_problem['names']))][::-1]
+for i in range(len(S1_results)):
+    print(i, S1_results[i][0], S1_results[i][1])
+    # print(i, S1_results[i][0])
+
+# +
+# Show S1 rankings
+
+ST_results = [(name, x) for x, name in sorted(zip(Si['ST'], sobol_problem['names']))][::-1]
+for i in range(len(ST_results)):
+    print(i, ST_results[i][0], ST_results[i][1])
+# -
+
+Si['S1']
+
+# +
+a = np.array([1, 0, 1, 1, 1, 1, 7, 0, 0])
+
+np.where(a != 0)[0]
+# -
+
+
--- ezuq/sobol.py
+++ ezuq/sobol.py
@@ -21,6 +21,8 @@ from ezuq.simulation.jsr import run_simulation
 
 CHUNK_SIZE = 1000
 
+CONFIDENCE_INTERVAL = 0.95
+
 def setup_runfiles(working_dir, conditions, morris_dir='?', i_sens=None, N=1024, SEED=400):
     """Set up the runfiles for Sobol Sampling
     working_dir should be the directory where the RMG and Cantera mechanisms are saved.
@@ -66,7 +68,41 @@ def setup_runfiles(working_dir, conditions, morris_dir='?', i_sens=None, N=1024,
     # make the condition dirs, use the input conditions to name things
     ezuq.util.setup_condition_dirs(sobol_dir, conditions)
     if morris_dir and i_sens is not None:
-        raise NotImplementedError('Sobol using model reduction not implemented yet')
+        # We are performing model reduction
+        print('Using Morris screening results to reduce the parameter space for Sobol sampling')
+
+        for condition in conditions:
+            morris_condition_dir = os.path.join(morris_dir, condition['name'])
+            sobol_condition_dir = os.path.join(sobol_dir, condition['name'])
+            shutil.copyfile(os.path.join(morris_condition_dir, 'morris_screen_set.yaml'), os.path.join(sobol_condition_dir, 'morris_screen_set.yaml'))
+            with open(os.path.join(sobol_condition_dir, 'morris_screen_set.yaml'), 'r') as f:
+                morris_screen_result = yaml.load(f, Loader=yaml.FullLoader)
+
+            g_params = morris_screen_result['g_params']
+            k_params = morris_screen_result['k_params']
+            g_param_names = morris_screen_result['g_param_names']
+            k_param_names = morris_screen_result['k_param_names']
+
+            alpha = (1 - CONFIDENCE_INTERVAL) / 2
+            problem = {
+                'num_vars': len(g_params) + len(k_params),
+                'names': g_param_names + k_param_names,
+                'bounds': [[alpha, 1 - alpha]] * (len(g_params) + len(k_params)),  # (slightly clipped) unit uniforms, we'll handle the actual translation to valid perturbations later on
+                'g_params': g_params,
+                'k_params': k_params,
+                'g_param_names': g_param_names,
+                'k_param_names': k_param_names,
+                'model_reduction': True
+            }
+            with open(os.path.join(sobol_condition_dir, 'problem_desc.yaml'), 'w') as f:
+                yaml.dump(problem, f, default_flow_style=False)
+
+            # Generate Sobol samples (takes a minute)
+            # these will be different for each condition because the reduced parameter set will be different
+            X = SALib.sample.sobol.sample(problem, N=N, calc_second_order=False, seed=SEED)
+            print(f'Generated {X.shape[0]} samples with {X.shape[1]} variables')
+            np.save(os.path.join(sobol_condition_dir, 'sobol_samples.npy'), X)
+
     else:
         print('No reduced model params provided, this will run sampling on the full set')
 
@@ -78,8 +114,7 @@ def setup_runfiles(working_dir, conditions, morris_dir='?', i_sens=None, N=1024,
 
         # Define the problem using SALib format
         # we need to clip the bounds to avoid infinity in the transformation to normal space.
-        confidence_interval = 0.95
-        alpha = (1 - confidence_interval) / 2
+        alpha = (1 - CONFIDENCE_INTERVAL) / 2
 
         problem = {
             'num_vars': len(species_list) + len(reaction_list),
@@ -173,14 +208,90 @@ def run_chunk(settings_yaml, chunk_index):
         print('Parameters are independent')
         # Figure out if there was any model reduction done
         if 'model_reduction' in problem and problem['model_reduction'] == True:
-            raise NotImplementedError('Sobol with model reduction not implemented yet')
+            sobol_condition_dir = os.path.dirname(problem_desc_file)
+
+            X = np.load(os.path.join(sobol_condition_dir, 'sobol_samples.npy'))
+            if chunk_index * CHUNK_SIZE >= X.shape[0]:
+                print(f"Chunk index {chunk_index} is out of range for number of samples {X.shape[0]} with chunk size {CHUNK_SIZE}")
+                exit(0)
+            
+            # Get our subset of samples for this chunk
+            maximum_index = min((chunk_index + 1) * CHUNK_SIZE, X.shape[0])
+            X = X[chunk_index * CHUNK_SIZE: maximum_index, :]
+
+            g_params = problem['g_params']
+            k_params = problem['k_params']
+            assert X.shape[1] == len(g_params) + len(k_params), "Number of variables in Sobol samples does not match number of species + reactions"
+
+
+            # Get the thermo perturbations
+            # ------------- Thermo perturbations -------------
+            thermo_uniform_perturbations = X[:, :len(g_params)]
+            L_thermo = np.linalg.cholesky(thermo_covariance_matrix)
+            assert np.isclose(L_thermo @ L_thermo.T, thermo_covariance_matrix).all()
+            z_thermo_reduced = scipy.stats.norm.ppf(thermo_uniform_perturbations)  # transform the unit uniforms to standard normals
+            z_thermo = np.zeros((z_thermo_reduced.shape[0], len(species_list)))
+
+            for i, sp_index in enumerate(g_params):
+                z_thermo[:, sp_index] = z_thermo_reduced[:, i]
+
+            thermo_perturbations = (L_thermo @ z_thermo.T).T * 4184  # convert RMG-UQ's kcal/mol to J/mol
+
+            # ------------- Kinetic perturbations -------------
+            kinetic_uniform_perturbations = X[:, len(g_params):]
+            L_kinetic = np.linalg.cholesky(kinetic_covariance_matrix)
+            assert np.isclose(L_kinetic @ L_kinetic.T, kinetic_covariance_matrix).all()
+            z_kinetic_reduced = scipy.stats.norm.ppf(kinetic_uniform_perturbations)
+            z_kinetic = np.zeros((z_kinetic_reduced.shape[0], len(reaction_list)))
+            for i, rxn_index in enumerate(k_params):
+                z_kinetic[:, rxn_index] = z_kinetic_reduced[:, i]
+
+            kinetic_perturbations = (L_kinetic @ z_kinetic.T).T  # these are the perturbations in log space, so we can exponentiate to get the kinetic multipliers
+            kinetic_multipliers_rmg = np.exp(kinetic_perturbations)
+            kinetic_multipliers_ct = kinetic_multipliers_rmg @ ct2rmg_matrix.T  # convert from RMG reaction space to Cantera reaction space
+
+            k_params_ct = np.where(kinetic_multipliers_ct[0, :] != 0)[0]
+            k_params_ct_check = np.where(kinetic_multipliers_ct[1, :] != 0)[0]
+            assert set(k_params_ct) == set(k_params_ct_check)
+            # Do the perturbations and run the simulations
+            # save copies of all thermo for faster perturbation
+            thermo_copies = []
+            for sp_index in range(gas.n_species):
+                thermo_copies.append(ct.Species().from_dict(gas.species()[sp_index].input_data.copy()))
+
+            # Cantera does well if you give it lots of CPUs for a single simulation
+            # but slows down if you try to parallelize different simulations across multiple processes
+            # so we run the simulations in serial here do the parallelize across SLURM array jobs.
+            for i in range(X.shape[0]):
+
+                # perturb all the species
+                for sp_index in g_params:
+                    perturbed_sp = ezuq.util.perturb_species_ct(gas.species()[sp_index], thermo_perturbations[i, sp_index])
+                    gas.modify_species(sp_index, perturbed_sp)
+
+                # set multipliers
+                for rxn_index_ct in k_params_ct:
+                    gas.set_multiplier(kinetic_multipliers_ct[i, rxn_index_ct], rxn_index_ct)
+                try:
+                    # TODO add timeout here so that if a simulation is taking too long we can skip it and move on 
+                    y[i, :] = run_simulation(gas, settings)
+                except ct.CanteraError:
+                    y[i, :] = np.nan  # if the simulation fails, we can fill in NaNs and move on. The Morris analysis can handle some failed simulations as long as most of them work.
+
+                # Reset things
+                for sp_index in g_params:
+                    gas.modify_species(sp_index, thermo_copies[sp_index])
+                gas.set_multiplier(1.0)
+
+            np.save(output_filename, y)
         else:
             # ------------- no model reduction, full sampling -------------
             X = np.load(os.path.join(sobol_dir, 'sobol_samples_unreduced.npy'))
             assert X.shape[1] == thermo_covariance_matrix.shape[0] + kinetic_covariance_matrix.shape[0], "Number of variables in Sobol samples does not match number of species + reactions"
 
             if chunk_index * CHUNK_SIZE >= X.shape[0]:
-                raise ValueError(f"Chunk index {chunk_index} is out of range for number of samples {X.shape[0]} with chunk size {CHUNK_SIZE}")
+                print(f"Chunk index {chunk_index} is out of range for number of samples {X.shape[0]} with chunk size {CHUNK_SIZE}")
+                exit(0)
     
             # Get our subset of samples for this chunk
             maximum_index = min((chunk_index + 1) * CHUNK_SIZE, X.shape[0])
@@ -217,7 +328,7 @@ def run_chunk(settings_yaml, chunk_index):
                 # perturb all the species
                 for sp_index in range(gas.n_species):
                     # random perturbation
-                    if thermo_perturbations[i, sp_index] != 0:
+                    if thermo_perturbations[i, sp_index] != 0:  # not sure I need this. We're sampling everything...
                         perturbed_sp = ezuq.util.perturb_species_ct(gas.species()[sp_index], thermo_perturbations[i, sp_index])
                         gas.modify_species(sp_index, perturbed_sp)
 
